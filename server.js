73a35db37048361f43b4f7ea5c459ea48fac932e
unknown <oneminutestudio2026@gmail.com>
2026-06-05 21:00:05 +0700

    fix: Express 5 wildcard route syntax

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'dist')))
 
 // SPA fallback - all routes serve index.html
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
